43c0530a648c3b2ea3874890dfbc230800d3de49
Gabriel <115580274+gabszs@users.noreply.github.com>
2025-04-09 19:45:46 +0000

    removin for tests

--- backend/api/server.ts
+++
@@ -1,4 +0,0 @@
-import app from ".";
-
-const server = app.listen(process.env.PORT || 3000);
-export default server;
--- backend/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-
-const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["./__tests__/teardown-users.ts"],
-};
-
-export default jestConfig;
